a233f2e5b27fb43a3e57b5ad03b308c12f7b9836
Jeremiah Akpera <jerryakpera@gmail.com>
2020-07-23 08:55:05 +0300

    v1.0.6

--- db/db.js
+++ db/db.js
@@ -6,19 +6,19 @@ const env = require('../config/env');
 const dbURL = env === 'prod' ? process.env.DBURL : config.dbURL;
 
 // Connect to DB with user and authentication
-mongoose.connect(config.dbURL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
+mongoose.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 const db = mongoose.connection;
 // check for database errors
 db.on('error', (err) => {
-	console.log(err);
+  console.log(err);
 });
 // If connected log connected to mongodb
 db.once('open', () => {
-	console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB');
 });
 
 module.exports = db;
